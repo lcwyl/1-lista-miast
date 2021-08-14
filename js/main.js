81a3de147dc9773414d8e8470de7888fc881b017
Łukasz Cwyl <lukasz.cwyl@commart.pl>
2021-08-14 21:18:00 +0200

    Nowe elementy listy numerowanej

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let cities = ['Warsaw', 'Berlin', 'Paris', 'London', 'New York', 'Tokio', 'Moscow'];
+
+let ol = document.createElement("ol");
+document.body.appendChild(ol);
+let li;
+cities.forEach(function (el) {
+    let li = document.createElement("li");//tworzymy elementy li
+    console.log(li)
+    li.innerText = el;//dodajemy text do li
+    li.className = el;//dodajemy klase do li
+
+    
+})
+ol.appendChild(li)//za każdą iteracją pętli dodajmy kolejny li do ol
